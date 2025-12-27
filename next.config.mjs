3ff626571a4517b0fc9eb0fd1247cb36a1ba4567
DEVIL0924 <bfmiop085@gmail.com>
2025-12-27 23:47:37 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  poweredByHeader: false,
+  reactStrictMode: true,
+  compress: true,
+  images: {
+    unoptimized: true
+  }
+};
 
 export default nextConfig;
